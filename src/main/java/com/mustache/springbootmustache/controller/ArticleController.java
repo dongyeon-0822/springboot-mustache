12d29200024c01bec989d581971e7306d1127246
DongYeon Kang <myjjue00@gmail.com>
2022-11-07 14:31:36 +0900

    Create ArticleController, ArticleDto

---
+++ src/main/java/com/mustache/springbootmustache/controller/ArticleController.java
@@ -0,0 +1,24 @@
+package com.mustache.springbootmustache.controller;
+
+import com.mustache.springbootmustache.domain.dto.ArticleDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articles")
+@Slf4j // 로깅을 위한 어노테이션 log를 사용할 수있다
+public class ArticleController {
+    @GetMapping(value = "/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping(value = "/posts")
+    public String createArticle(ArticleDto form) {
+        log.info(form.toString());
+        return "";
+    }
+}
